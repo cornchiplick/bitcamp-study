de00ea3471087b12e64deefae252712ba7d881ba
cornchiplick <ilsunkiss@gmail.com>
2022-07-05 17:17:32 +0900

    백업2

---
+++ java-lang/src/Exam06.java
@@ -0,0 +1,8 @@
+public class Exam06 {
+    public static void main(String[] args) {
+        System.out.println(true); //무조건 대소문자 구분
+        System.out.println(false); //무조건 대소문자 구분
+        System.out.println(10 == 11);
+        System.out.println(10 < 11);
+    }
+}
