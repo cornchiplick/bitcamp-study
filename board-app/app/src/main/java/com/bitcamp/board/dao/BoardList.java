981b14d56c3d63c54445ac25cf458085eeaed3c4
cornchiplick <ilsunkiss@gmail.com>
2022-07-29 19:39:04 +0900

    20220729

--- board-app/app/src/main/java/com/bitcamp/board/dao/BoardList.java
+++ board-app/app/src/main/java/com/bitcamp/board/dao/BoardList.java
@@ -30,8 +30,15 @@ public class BoardList extends ObjectList {
   // 서브 클래스의 get() 메서드에서 처리할 상황이 아니다.
   // 서브 클래스의 get()을 호출한 쪽에 보고하는 것이 더 낫다.
   // 이럴경우 try ~ catch ~ 를 쓰지 말고 메서드 선언부에 발생되는 예외를 표시하라!
+
+  // ObjectList의 get()에서 던지는 예외를 이 메서드에서 처리하지 않고
+  // 호출자에게 처리를 위임한다.
+  // => ListException은 RuntimeException 계열이기 때문에
+  //    메서드 선언부에 표시하지 않아도 된다.
+  //    Exception 계열의 예외를 다루는 것 보다 덜 번거롭다.
+
   @Override
-  public Board get(int boardNo) throws Throwable {
+  public Board get(int boardNo) {
     for (int i = 0; i < size(); i++) {
       Board board = (Board) super.get(i);
       if (board.no == boardNo) {
@@ -46,7 +53,7 @@ public class BoardList extends ObjectList {
   // 게시글 번호의 항목을 삭제하도록 상속 받은 메서드를 재정의 한다.
 
   @Override
-  public boolean remove(int boardNo) throws Throwable {
+  public boolean remove(int boardNo) {
     for (int i = 0; i < size(); i++) {
       Board board = (Board) super.get(i);
       if (board.no == boardNo) {
