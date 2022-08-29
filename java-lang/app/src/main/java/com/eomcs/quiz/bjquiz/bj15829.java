6e7fca2fc59384db8d2a06e3b65f78c4cd22eac5
cornchiplick <ilsunkiss@gmail.com>
2022-08-29 20:33:50 +0900

    20220829

---
+++ java-lang/app/src/main/java/com/eomcs/quiz/bjquiz/bj15829.java
@@ -0,0 +1,37 @@
+package com.eomcs.quiz.bjquiz;
+
+import java.util.Scanner;
+
+public class bj15829 {
+  public static void main(String[] args) {
+
+    Scanner in = new Scanner(System.in);
+
+    int len = in.nextInt();
+    String str = in.next();
+    int h = 0;
+
+
+    for (int i = 0; i < len; i++) {
+      h += (str.charAt(i) - 'a' + 1) * ((int) Math.pow(31, i));
+    }
+
+    System.out.println(h%1234567891);
+  }
+
+  // 중단점
+  static int ismod(int op, int i) {
+
+    if (i == 0) {
+      return op;
+    }
+
+    int re = op;
+    for (int j = 0; j < i; j++) {
+      re = (re*31) % 1234567891;
+    }
+
+    return 0;
+  }
+
+}
