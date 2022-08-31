1e8fd1c5bf9214d2a0604d2f9882926ddb68f70a
cornchiplick <ilsunkiss@gmail.com>
2022-08-31 20:34:11 +0900

    20220831

--- java-lang/app/src/main/java/com/eomcs/quiz/bjquiz/Main.java
+++ java-lang/app/src/main/java/com/eomcs/quiz/bjquiz/Main.java
@@ -1,19 +1,46 @@
 package com.eomcs.quiz.bjquiz;
 
-import java.util.Scanner;
-
 public class Main {
 
   public static void main(String[] args) {
 
+    /*
     Scanner in = new Scanner(System.in);
 
     int N = in.nextInt();
 
     int[] arr = new int[N];
 
+    for (int i = 0; i < N; i++) {
+      arr[i] = in.nextInt();
+    }
 
+    int count = 0;
+
+    while (count < N) {
+
+      for(int i=1; i < arr.length; i++){
+        for(int j=i ; j >= 1; j--){
+
+          if(arr[j] < arr[j-1]){  //한 칸씩 왼쪽으로 이동
+            int tmp = arr[j];
+            arr[j] = arr[j-1];
+            arr[j-1] = tmp;
+          } else break;  //자기보다 작은 데이터를 만나면 그 위치에서 멈춤
+
+        }
+      }
+
+      count++;
+    } // while
+
+    for (int i = 0; i < N; i++) {
+      System.out.println(arr[i]);
+    }
+
+     */
 
   }
+
 
 }
