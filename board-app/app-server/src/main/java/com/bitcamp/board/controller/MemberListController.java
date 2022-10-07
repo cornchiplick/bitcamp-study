fea2ea12362bede80f9ba8dac48a3191d9fd9979
cornchiplick <ilsunkiss@gmail.com>
2022-10-07 18:08:55 +0900

    edit: Facade Design Pattern apply

--- board-app/app-server/src/main/java/com/bitcamp/board/controller/MemberListController.java
+++ board-app/app-server/src/main/java/com/bitcamp/board/controller/MemberListController.java
@@ -1,40 +1,27 @@
 package com.bitcamp.board.controller;
 
-import java.io.IOException;
 import java.util.List;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import com.bitcamp.board.domain.Member;
 import com.bitcamp.board.service.MemberService;
+import com.bitcamp.servlet.Controller;
 
-@WebServlet("/member/list")
-public class MemberListController extends HttpServlet {
-  private static final long serialVersionUID = 1L;
+public class MemberListController implements Controller {
 
   MemberService memberService;
 
-  @Override
-  public void init() {
-    memberService = (MemberService) this.getServletContext().getAttribute("memberService");
+  public MemberListController(MemberService memberService) {
+    this.memberService = memberService;
   }
 
   @Override
-  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-      throws ServletException, IOException {
-    try {
-      List<Member> members = memberService.list();
+  public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    List<Member> members = memberService.list();
 
-      req.setAttribute("members",  members);
-      resp.setContentType("text/html;charset=UTF-8");
-      req.getRequestDispatcher("/member/list.jsp").include(req, resp);
+    request.setAttribute("members",  members);
+    return "/member/list.jsp";
 
-    } catch (Exception e) {
-      req.setAttribute("exception", e);
-      req.getRequestDispatcher("/error.jsp").forward(req, resp);
-    }
   }
 }
 
