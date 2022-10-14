0e9f3590f7818e6ea3a50e28e053f0d3b805c98a
cornchiplick <ilsunkiss@gmail.com>
2022-10-14 17:01:38 +0900

    20221014; update: root ioc container, update: try a lot of initializer

--- board-app/app-server/src/main/java/com/bitcamp/board/config/AppConfig.java
+++ board-app/app-server/src/main/java/com/bitcamp/board/config/AppConfig.java
@@ -27,8 +27,14 @@ public class AppConfig {
     System.out.println("AppConfig() 생성자 호출됨!");
   }
 
-  @Bean("transactionManager")
-  public PlatformTransactionManager createTransactionManager(DataSource ds) {
+  // @Bean 애노테이션을 붙일 떄 객체 이름을 지정하면
+  // 그 이름으로 리턴 값을 컨테이너에 보관한다.
+  // 이름을 지정하지 않으면 메서드 이름으로 보관한다.
+  //  @Bean("transactionManager")
+  // 이름을 생략하면 메서드 명을 객체 이름으로 사용한다.
+  // 그래서 동사구 형태가 아닌 명사형태의 메서드 명을 사용한다.
+  @Bean
+  public PlatformTransactionManager transactionManager(DataSource ds) {
     // Spring IoC 컨테이너는 이 메서드를 호출하기 전에 먼저
     // 이 메서드가 원하는 파라미터 값인 DataSource를 먼저 생성한다.
     // => createDataSource() 메서드를 먼저 호출한다.
@@ -39,8 +45,8 @@ public class AppConfig {
 
   // DataSource 를 생성하는 메서드를 호출하라고 표시한다.
   // 메서드가 리턴한 객체는 @Bean 애노테이션에 지정된 이름으로 컨테이너에 보관될 것이다.
-  @Bean("dataSource")
-  public DataSource createDataSource() {
+  @Bean
+  public DataSource dataSource() {
     System.out.println("createDataSource() 호출됨!");
 
     DriverManagerDataSource ds = new DriverManagerDataSource();
@@ -54,14 +60,14 @@ public class AppConfig {
   // multipart/form-data 형식으로 보내온 요청 데이터를
   // 도메인 객체로 받는 일을 할 도우미 객체를 등록한다.
   // 이 객체가 등록된 경우 multipart/form-data 를 도메인 객체로 받을 수 있다.
-  @Bean("multipartResolver")
-  public MultipartResolver createMultipartResolver() {
+  @Bean
+  public MultipartResolver multipartResolver() {
     return new StandardServletMultipartResolver();
   }
 
   // Spring WebMVC 의 기본 ViewResolver를 교체한다.
-  @Bean("viewResolver")
-  public ViewResolver createViewResolver() {
+  @Bean
+  public ViewResolver viewResolver() {
     InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
     viewResolver.setViewClass(JstlView.class); // 주어진 URL을 처리할 객체 => JSP를 실행시켜주는 객체
     viewResolver.setPrefix("/WEB-INF/jsp/");
